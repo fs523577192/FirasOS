71ba7fe38d70542b75c9e76500f03031f6981eac
Firas <fs448445223@126.com>
2014-09-22 23:48:55 +0800

    Little progress on data structures

---
+++ DataStructure/SortedBinaryTree.cpp
@@ -0,0 +1,8 @@
+#ifndef _SORTED_BINARY_TREE_H_
+#define _SORTED_BINARY_TREE_H_
+#include "Collection.cpp"
+template <typename T> class SortedBinaryTree : public SortedCollection{
+public:
+	
+}; // class SortedBinaryTree<T>
+#endif // _SORTED_BINARY_TREE_H_
