395dae9e8c9e6a167b84ce50d47a6563a27835be
Firas <fs448445223@126.com>
2014-11-10 00:41:59 +0800

    Seperate codes for performance and for readability

--- DataStructure/SortedBinaryTree.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef _SORTED_BINARY_TREE_H_
-#define _SORTED_BINARY_TREE_H_
-#include "Collection.cpp"
-template <typename T> class SortedBinaryTree : public SortedCollection<T>{
-public:
-	
-}; // class SortedBinaryTree<T>
-#endif // _SORTED_BINARY_TREE_H_
--- DataStructure/testRedBlackTree.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "RedBlackTree.cpp"
-int main(){
-	RedBlack<double> *tree = new RedBlack<double>();
-	std::cout << tree->isEmpty() << '\t' << tree->getSize() << '\n';
-	
-	return 0;
-}
